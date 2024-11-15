f9b5373e546d1a8f31b79b32108780177bd9961e
adhi05-boop <badithyan25@gmail.com>
2024-11-15 21:16:24 +0530

    done

--- adithyan.sh
+++ adithyan.sh
@@ -1,5 +1,14 @@
-#! /bin/bash
+#!/bin/bash
 echo $(pwd)
 echo "enter your name"
 read n1
 echo hello $n1
+echo "enter the path"
+read n2
+if [ -f "$n2" ]; then
+	echo "$n2 is a file"
+elif [ -d "$n2" ]; then
+	echo "$n2 is dic"
+else 
+	echo "$n2 something"
+fi
